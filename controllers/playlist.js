e4b2d812e159e11c9703a5737b52647d6e98cf32
Dpalme <diegoPalmerin24@gmail.com>
2021-10-30 21:24:58 -0500

    fixed http request status

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -7,10 +7,10 @@ exports.postCreatePlaylist = async (req, res) => {
     try {
         await element.save()
         console.log("Status 200: Added successfully")
-        res.send({ status: 200, message: "Added successfully" })
+        res.status(200).json({ status: 200, message: "Added successfully" })
     } catch (err) {
         console.log(err)
-        res.send({ status: 422, message: "An error occurred" })
+        res.status(422).json({ status: 422, message: "An error occurred" })
     }
 }
 
@@ -23,10 +23,10 @@ exports.postUpdatePlaylist = async (req, res) => {
     try {
         await Playlist.findOneAndUpdate(req.body.filter, req.body.update)
         console.log("Status 200: Updated successfully")
-        res.send({ status: 200, message: "Updated successfully" })
+        res.status(200).send({ status: 200, message: "Updated successfully" })
     } catch (err) {
         console.log(err)
-        res.send({ status: 422, message: "An error occurred" })
+        res.status(422).send({ status: 422, message: "An error occurred" })
     }
 }
 
@@ -34,9 +34,9 @@ exports.postDeletePlaylist = async (req, res) => {
     try {
         await Playlist.findOneAndRemove(req.body)
         console.log("Status 200: Deleted successfully")
-        res.send({ status: 200, message: "Deleted successfully" })
+        res.status(200).send({ status: 200, message: "Deleted successfully" })
     } catch (err) {
         console.log(err)
-        res.send({ status: 422, message: "An error occurred" })
+        res.status(422).send({ status: 422, message: "An error occurred" })
     }
 }
