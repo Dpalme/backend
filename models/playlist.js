d838b0b20481cca09e6bbddcec5a4fb22f56eff2
Dpalme <diegoPalmerin24@gmail.com>
2021-10-30 21:21:48 -0500

    generalized playlist

--- models/playlist.js
+++ models/playlist.js
@@ -7,13 +7,6 @@ const PlaylistSchema = mongoose.Schema({
         maxLength: [50, "Error 422: InvalidBodyException. Title must be between 5 and 50 characters"],
         minLenght: [5, "Error 422: InvalidBodyException. Title must be between 5 and 50 characters"],
     },
-    type: {
-        type: [String],
-        enum: {
-            values: ['Book', 'Song'],
-            message: 'Error 422: InvalidBodyException. Must choose from one of these types: Book or Song'
-        }
-    },
     author: {
         type: [String, "Error 422: InvalidBodyException.	Field must be string"],
         maxLength: [50, "Error 422: InvalidBodyException. Author must be between 5 and 50 characters"],
